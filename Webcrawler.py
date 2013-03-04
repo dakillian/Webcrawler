975b126f546489285026b3e554beedb98a850a21
AndrrewK <greenbindude@gmail.com>
2013-03-03 21:52:28 -0400

    Update Webcrawler.py
    rewrote a lot of the code

--- Webcrawler.py
+++ Webcrawler.py
@@ -1,33 +1,32 @@
-import urllib, pickle
+import urllib
+
+page = urllib.urlopen("http://www.python.org")
+
+page_contents = page.read()
 
 link_list = []
-
-def get_first_link(page):
-	page = urllib.urlopen(page)
-	global page_contents
-	page_contents = page.read()
-	#page.close()
+	
+def get_first_link(page_contents):
 	link_tag = page_contents.find("<a href=")
 	start_quote = page_contents.find('"', link_tag)
-	#global end_quote
 	end_quote = page_contents.find('"', start_quote + 1)
 	url = page_contents[start_quote + 1:end_quote]
 	link_list.append(url)
-	#print link_list
-	get_next_link("http://www.python.org", end_quote)
-
-"""maybe pass page_contents as a perameter to get_next_link instead of making page_contents a global variable"""
-def get_next_link(page, end_quote):
+	get_next_link(page_contents, end_quote)
+	    
+def get_next_link(page_contents, end_quote):
 	link_tag = page_contents.find("<a href=", end_quote)
 	while not link_tag == -1:
+		link_tag = page_contents.find("<a href=", end_quote)
 		start_quote = page_contents.find('"', link_tag)
-		end_qoute = page_contents.find('"', start_quote)
-		url = page_contents[start_quote + 1:end_quote]
-		link_list.append(url)
-		pickle.dump(link_list, open('links', 'wb'))
-		#print link_list
-		print url
-		get_next_link("http://www.python.org", end_quote)
-	
-
-get_first_link("http://www.python.org") 
+		end_quote = page_contents.find('"', start_quote + 1)
+		if link_tag == -1:
+			page_contents = urllib.urlopen(link_list)
+			get_first_link(page_contents)
+		else:	
+			url = page_contents[start_quote + 1:end_quote]
+			link_list.append(url)
+			print url
+	#get_next_link(page_contents, end_quote)
+	    
+get_first_link(page_contents) 
